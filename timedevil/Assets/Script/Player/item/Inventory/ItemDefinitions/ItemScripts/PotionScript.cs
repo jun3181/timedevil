207e916f7c49131deabd4b1f7411061d3c5ebc2b
PermanentNostalgia <140535913+PermanentNostalgia@users.noreply.github.com>
2026-05-15 11:40:30 +0900

    feature: 포션 사용 스크립트

--- timedevil/Assets/Script/Player/item/Inventory/ItemDefinitions/ItemScripts/PotionScript.cs
+++ timedevil/Assets/Script/Player/item/Inventory/ItemDefinitions/ItemScripts/PotionScript.cs
@@ -4,12 +4,58 @@ using UnityEngine;
 
 public class PotionScript : ItemScriptBase
 {
+    [Header("최대 체력 변화량")]
+    [SerializeField]
+    private int deltaMaxHP = 0;
+
+    [Header("체력 변화량")]
+    [SerializeField]
+    private int deltaCurrentHP = 0;
+
+    [Header("공격력 변화량")]
+    [SerializeField]
+    private int deltaAttack = 0;
+
+    [Header("방어력 변화량")]
+    [SerializeField]
+    private int deltaDefense = 0;
+
+    [Header("속력 변화량")]
+    [SerializeField]
+    private int deltaSpeed = 0;
+
+    [Header("디버그 메시지 출력")]
+    [SerializeField]
+    private bool debuged = false;
+
     public override void Run() {
-        Debug.Log("루시의 스탯");
-        if(PlayerDataRuntime.Instance) {
-            Debug.Log($"currentHP: {PlayerDataRuntime.Instance.Data.currentHP}");
+        PlayerData player = PlayerDataRuntime.Instance.Data;
+            
+        player.maxHP += deltaMaxHP;
+        if(player.maxHP < 0) player.maxHP = 0;
+
+        player.currentHP += deltaCurrentHP;
+        if(player.currentHP>player.maxHP) player.currentHP = player.maxHP;
+
+        player.attack += deltaAttack;
+        if(player.attack < 0) player.attack = 0;
+
+        player.defense += deltaDefense;
+        if(player.defense < 0) player.defense = 0;
+        
+        player.speed += deltaSpeed;
+        if(player.speed < 1) player.speed = 1;
+
+        if(debuged) {
+            Debug.Log($"최대 체력: {player.maxHP}, 현제 체력: {player.currentHP}, 공격력: {player.attack}, 방어력: {player.defense}, 속력: {player.speed}");
+        }
+    }
+
+    public override bool CanItemUsed() {
+        if(PlayerDataRuntime.Instance==null) {
+            return false;
         } else {
-            Debug.Log("와피스");
+            return true;
         }
     }
 }
