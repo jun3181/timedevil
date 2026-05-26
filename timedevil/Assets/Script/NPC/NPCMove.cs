07e416fc501dc4baba171e7d916a8460eba98a99
PermanentNostalgia <140535913+PermanentNostalgia@users.noreply.github.com>
2026-05-26 20:15:17 +0900

    fix: 플레이어가 NPC에 밀려나는 현상

--- timedevil/Assets/Script/NPC/NPCMove.cs
+++ timedevil/Assets/Script/NPC/NPCMove.cs
@@ -23,19 +23,27 @@ public class NPCMove : MonoBehaviour
     private Rigidbody2D rb;
     private Collider2D collider;
     private int movementPriority;
-    private Vector3 colliderDetectMargin;
 
     private Vector2 startPos;
     private Vector2 velocity;
     private float takingTime;
+
+    private Vector2 colliderGlobalOffset = new();
+    // private Vector2 collisionDetectionPadding = new(0.02f, 0.02f);
+    /* 이동할 영역에 콜라이더가 있으면 NPC는 정지하는데,
+     * 콜라이더를 감지할 영역을 축소하는 역할을 함
+     * 패딩을 적용할 경우 플레이어가 옆에서 부딪힐 때 정지하지 않음
+     */
+
     void Awake() {
         movementPriority = ++NPCCount;
 
         rb = GetComponent<Rigidbody2D>();
         rb.bodyType = RigidbodyType2D.Kinematic;
         collider = GetComponent<Collider2D>();
-
-        colliderDetectMargin = new(1, 1);
+        
+        colliderGlobalOffset.x = collider.offset.x * transform.localScale.x;
+        colliderGlobalOffset.y = collider.offset.y * transform.localScale.y;
     }
 
     void FixedUpdate() {
@@ -44,17 +52,16 @@ public class NPCMove : MonoBehaviour
             float takenTime = (newPos - startPos).magnitude / Speed;
 
             Collider2D[] results = new Collider2D[2];
-            Vector3 colliderCenter = (Vector3)newPos + (Vector3)collider.offset;
-            int newContactCount = Physics2D.OverlapAreaNonAlloc(colliderCenter + collider.bounds.extents + colliderDetectMargin, colliderCenter - collider.bounds.extents - colliderDetectMargin, results);
-            Debug.Log(newContactCount);
-            if(newContactCount>1) {
+            Vector2 newColliderCenter = newPos + colliderGlobalOffset;
+            int newContactCount = Physics2D.OverlapAreaNonAlloc(newColliderCenter + (Vector2)collider.bounds.extents, newColliderCenter - (Vector2)collider.bounds.extents, results);
+            if(newContactCount > 1) {
                 if(debuged) Debug.Log($"{gameObject.name}이 {GetPosition()}위치에서 충돌을 회피하기 위해 움직임을 멈춤");
                 if(CanStandbyForAvoiding) {
                     Idle();
                 } else {
                     Stop();
                 }
-            } else if(takenTime >= takingTime || newPos==rb.position) {
+            } else if(takenTime >= takingTime || newPos == rb.position) {
                 rb.MovePosition(startPos + velocity * takingTime);
                 Stop();
             } else {
