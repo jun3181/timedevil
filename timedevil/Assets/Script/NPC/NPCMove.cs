70af228a01cfbd49655bbca62005c8227b095188
jun3181 <rlatjdwns0109@gmail.com>
2026-06-04 23:30:03 +0900

    Update NPCMove.cs

--- timedevil/Assets/Script/NPC/NPCMove.cs
+++ timedevil/Assets/Script/NPC/NPCMove.cs
@@ -44,6 +44,8 @@ public class NPCMove : MonoBehaviour
     private int appliedHAxisRaw;
     private int appliedVAxisRaw;
     private bool appliedIsChange;
+    private bool continuousAnimationMove;
+    private bool continuousAnimationApplied;
     private Coroutine idleAnimationCoroutine;
 
     private Vector2 colliderGlobalOffset = new();
@@ -114,7 +116,15 @@ public class NPCMove : MonoBehaviour
         isPausingForAvoiding = false;
         UpdateLastDirectionFromVelocity();
         Moving = true;
-        ApplyMoveAnimation(true);
+
+        if(continuousAnimationMove) {
+            if(!continuousAnimationApplied) {
+                ApplyMoveAnimation(true);
+                continuousAnimationApplied = true;
+            }
+        } else {
+            ApplyMoveAnimation(true);
+        }
     }
 
     // 주어진 좌표로 이동
@@ -126,6 +136,7 @@ public class NPCMove : MonoBehaviour
     public void Idle() {
         Moving = false;
         isPausingForAvoiding = true;
+        continuousAnimationApplied = false;
         CancelPendingIdleAnimation();
         ApplyMoveAnimation(false);
     }
@@ -135,6 +146,9 @@ public class NPCMove : MonoBehaviour
         Moving = false;
         isPausingForAvoiding = false;
         takingTime = 0;
+
+        if(continuousAnimationMove) return;
+
         ScheduleIdleAnimation();
     }
 
@@ -164,6 +178,23 @@ public class NPCMove : MonoBehaviour
         isPausingForAvoiding = false;
         Moving = true;
         ApplyMoveAnimation(true);
+    }
+
+    public void BeginContinuousAnimationMove() {
+        CancelPendingIdleAnimation();
+        isPausingForAvoiding = false;
+        continuousAnimationMove = true;
+        continuousAnimationApplied = false;
+    }
+
+    public void EndContinuousAnimationMove(bool setIdle = true) {
+        continuousAnimationMove = false;
+        continuousAnimationApplied = false;
+        CancelPendingIdleAnimation();
+
+        if(setIdle) {
+            ApplyMoveAnimation(false);
+        }
     }
 
 
