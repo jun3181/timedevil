7111fd163bc7b003ca45a7adedd611b264276f19
PermanentNostalgia <140535913+PermanentNostalgia@users.noreply.github.com>
2026-07-03 16:34:14 +0900

    add: BoxHideout 소리 설정

--- timedevil/Assets/Script/Interactable/BoxHideout.cs
+++ timedevil/Assets/Script/Interactable/BoxHideout.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer), typeof(Collider2D))]
 public class BoxHideout : MonoBehaviour, IInteractable
 {
-    [Header("상자가 닫혔을 때 | 상자가 열렸을 때")]
+    private static PlayerMove playerMove = null;
+
+    [Header("상자가 열려있을 때 | 닫혀있을 때")]
     [SerializeField] private Sprite openedBoxSprite;
     [Tooltip("지정하지 않을 경우 현재 적용된 스프라이트 사용")]
     [SerializeField] private Sprite closedBoxSprite;
+
+    [Header("상자를 여는 소리 | 닫는 소리")]
+    [SerializeField] private AudioClip openingBoxSound;
+    [SerializeField] private AudioClip closingBoxSound;
 
     [SerializeField]
     [Header("디버그 메시지")]
     private bool debuged = false;
 
     private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
 
     void Awake()
     {
@@ -33,9 +40,15 @@ public class BoxHideout : MonoBehaviour, IInteractable
             }
             closedBoxSprite = spriteRenderer.sprite;
         }
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnEnable() {
+        if(playerMove == null)
+            playerMove = GameObject.FindWithTag("Player").GetComponent<PlayerMove>();
     }
 
     public void Interact() {
-        Debug.Log($"{gameObject.name}은 발동되었습니다. State: {enabled}");
+        if(!enabled) return;
     }
 }
