5584205e5e2c474c47fd0f785564c38a7f16911b
PermanentNostalgia <140535913+PermanentNostalgia@users.noreply.github.com>
2026-07-06 16:26:21 +0900

    chore: NPCPatrollerController 중간 저장

--- timedevil/Assets/Script/NPC/NPCPatrollerController.cs
+++ timedevil/Assets/Script/NPC/NPCPatrollerController.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class NPCPatrollerController : MonoBehaviour
 {
     public static NPCPatrollerController instance;
 
+    private static GameObject player;
+    private static Rigidbody2D playerRigidbody2D;
+
     [SerializeField]
     [Header("정찰병 프리팹")]
     private GameObject troopPrefab;
 
-    [Header("병사 스폰 최소/최대 시간 간격")]
-    [Range(0, 30)]
-    [SerializeField] private float minSpawnInterval = 8f;
-    [SerializeField] private float maxSpawnInterval = 27f;
+    [Header("주기적인 순찰병 스폰 최소/최대 시간 간격")]
+    [Range(0f, 30f)]
+    [SerializeField] private float minRegularSpawnInterval = 8f;
+    [SerializeField] private float maxRegularSpawnInterval = 27f;
+
+    [Header("직접적인 순찰병 스폰 최대 딜레이")]
+    [Range(0f, 10f)]
+    [SerializeField] private float maxInstantSpawnInterval = 3f;
+
+    [Header("순찰병 스폰시 플래이어와 X좌표 차 범위")]
+    [SerializeField] private float minDeltaXWithPlayer = 10f;
+    [SerializeField] private float maxDeltaXWithPlayer = 15f;
+
+    [Header("순찰병 스폰 Y좌표 범위")]
+    [SerializeField] private float minSpawnYPosition = 1.5f;
+    [SerializeField] private float maxSpawnYPosition = -3f;
 
     private GameObject[] instantiatedTroops = new GameObject[5];
     private Queue<GameObject> disappearedTroops = new();
@@ -29,6 +45,9 @@ public class NPCPatrollerController : MonoBehaviour
 
         instance = this;
 
+        player = GameObject.FindWithTag("Player");
+        playerRigidbody2D = player.GetComponent<Rigidbody2D>();
+
         NPCPatroller.OnDisappearing += TroopDisappearingEventHandler;
 
         for(int i=0; i<instantiatedTroops.Length; i++) {
@@ -37,11 +56,13 @@ public class NPCPatrollerController : MonoBehaviour
         }
     }
 
-    public void StartSpawningRepeatedly() {
-        if(spawningCoroutine != null) return;
+    public bool StartSpawningRepeatedly() {
+        if(spawningCoroutine != null) return false;
 
-        spawningCoroutine = SpawnTroopRepeatedly();
+        spawningCoroutine = SpawnRepeatedly();
         StartCoroutine(spawningCoroutine);
+
+        return true;
     }
 
     public void StopSpawningRepeatedly() {
@@ -51,12 +72,58 @@ public class NPCPatrollerController : MonoBehaviour
         }
     }
 
-    private IEnumerator SpawnTroopRepeatedly() {
-        float routineInterval;
+    public bool SpawnDirectly() {
+        if(disappearedTroops.Count == 0) return false;
+
+        GameObject troop = disappearedTroops.Dequeue();
+    }
+
+    private IEnumerator SpawnRepeatedly() {
+        Vector2 startPoint = new();
+        float routineInterval, deltaXWithPlayer;
+        GameObject troop;
+
         while(true) {
-            routineInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
-            
+            while(disappearedTroops.Count == 0) {
+                yield return null;
+            }
+
+            troop = disappearedTroops.Dequeue();
+            routineInterval = Random.Range(minRegularSpawnInterval, maxRegularSpawnInterval);
+
+            yield return new WaitForSeconds(routineInterval);
+
+            deltaXWithPlayer = Random.Range(minDeltaXWithPlayer, maxDeltaXWithPlayer);
+            if(Random.Range(0,2)==0) {
+                startPoint.x = playerRigidbody2D.position.x - deltaXWithPlayer;
+            } else {
+                startPoint.x = playerRigidbody2D.position.x + deltaXWithPlayer;
+            }
+            startPoint.y = Random.Range(minSpawnYPosition, maxSpawnYPosition);
+
+            troop.SetActive(true);
+
+            troop.GetComponent<Rigidbody2D>().MovePosition(startPoint);
+            troop.GetComponent<NPCPatroller>().Move();
         }
+    }
+
+    private IEnumerator DelaySpawn(GameObject troop, float delay) {
+        yield return new WaitForSeconds(delay);
+
+        Vector2 startPoint = new();
+        float deltaXWithPlayer = Random.Range(minDeltaXWithPlayer, maxDeltaXWithPlayer);
+        if(Random.Range(0, 2) == 0) {
+            startPoint.x = playerRigidbody2D.position.x - deltaXWithPlayer;
+        } else {
+            startPoint.x = playerRigidbody2D.position.x + deltaXWithPlayer;
+        }
+        startPoint.y = Random.Range(minSpawnYPosition, maxSpawnYPosition);
+
+        troop.SetActive(true);
+
+        troop.GetComponent<Rigidbody2D>().MovePosition(startPoint);
+        troop.GetComponent<NPCPatroller>().Move();
     }
 
     private void TroopDisappearingEventHandler(int id) {
